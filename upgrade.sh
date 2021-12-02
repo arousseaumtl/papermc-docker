cb311407316ef7eeada635b64d24893e595e91e0
arousseaumtl <92953733+arousseaumtl@users.noreply.github.com>
2021-12-02 12:53:40 -0500

    Updated

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-if docker images 2>&1 | grep -q 'papermc'; then docker run -it papermc /srv/papermc/compile.sh; else docker build .; fi
+if docker images 2>&1 | grep -q 'papermc';
+ then docker run papermc /bin/bash -c "pacman -Syu --noconfirm && \
+  /home/minecraft/srv/papermc/compile.sh & \
+  sleep 30 && killall java";
+ else docker build .;
+fi
